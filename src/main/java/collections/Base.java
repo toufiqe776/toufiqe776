48295bed06f945c85f5a8edf4ef64cfc3344dd5f
toufiq.nt@gmail.com <toufiq.nt@gmail.com>
2025-05-15 23:21:24 +0530

    Comments remove form base class

--- src/main/java/collections/Base.java
+++ src/main/java/collections/Base.java
@@ -2,17 +2,17 @@ package collections;
 
 public class Base {
     public static void main(String[]args){
-//        Person p=new Engineer();
-//        p.work();
-//       container c=new container();
-//      c.setLiquid("800");
-//      System.out.println(c.getLiquid() );
-//        car c=new car();
-//        c.start();
-//        c.stop();
-//        bike b=new bike();
-//        b.start();
-//        b.stop();
+        Person p=new Engineer();
+        p.work();
+       container c=new container();
+      c.setLiquid("800");
+      System.out.println(c.getLiquid() );
+        car c=new car();
+        c.start();
+        c.stop();
+        bike b=new bike();
+        b.start();
+        b.stop();
         Car1 c=new Car1();
         System.out.println(c.maxSpped);
         c.start();
