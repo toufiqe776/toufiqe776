9ca06e246d6287a9d0fd74ebea21eef52cbdad8a
toufiq.nt@gmail.com <toufiq.nt@gmail.com>
2025-05-21 11:05:30 +0530

    'Reverse number program using stringBuilder and StringBuffer class

--- src/main/java/codingPractice/ReverseNumber1.java
+++ src/main/java/codingPractice/ReverseNumber1.java
@@ -1,4 +1,17 @@
 package codingPractice;
 
+import java.util.Scanner;
+
 public class ReverseNumber1 {
+    public static void main(String[]args){
+        System.out.println("Enter number :");
+        Scanner sc=new Scanner(System.in);
+        int num =sc.nextInt();
+        int rev=0;
+        while(num!=0){
+            rev=rev*10+num%10;
+            num=num/10;
+        }
+        System.out.println("Reverse Number :"+rev);
+    }
 }
--- src/main/java/codingPractice/ReverseNumber2.java
+++ src/main/java/codingPractice/ReverseNumber2.java
@@ -1,4 +1,14 @@
 package codingPractice;
 
 public class ReverseNumber2 {
+    public static void main(String[]args){
+       /* StringBuffer rev;
+        StringBuffer sb=new StringBuffer(String.valueOf(123));*/
+
+       StringBuilder sb=new StringBuilder();
+       sb.append(123);
+       StringBuilder rev=sb.reverse();
+
+        System.out.println(rev);
+    }
 }
